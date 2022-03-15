3a4653c294207489a6397c756e3fc237ebe59205
Brandon High <highb@users.noreply.github.com>
2022-03-15 14:50:03 -0700

    Configure latest Z releases for binaries

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,8 +2,7 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for teleport-ent.
-GH_REPO="https://github.com/gravitational/teleport"
+REPO="https://get.gravitational.com/"
 TOOL_NAME="teleport-ent"
 TOOL_TEST="tsh version"
 
@@ -14,26 +13,15 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if teleport-ent is not hosted on GitHub releases.
-if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-  curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
-fi
-
 sort_versions() {
   sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
     LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
 
-list_github_tags() {
-  git ls-remote --tags --refs "$GH_REPO" |
-    grep -o 'refs/tags/.*' | cut -d/ -f3- |
-    sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
-}
-
 list_all_versions() {
-  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-  # Change this function if teleport-ent has other means of determining installable versions.
-  list_github_tags
+  echo "v8.3.4-darwin-amd64"
+  echo "v7.3.17-darwin-amd64"
+  echo "v6.2.31-darwin-amd64"
 }
 
 download_release() {
@@ -41,8 +29,7 @@ download_release() {
   version="$1"
   filename="$2"
 
-  # TODO: Adapt the release URL convention for teleport-ent
-  url="$GH_REPO/archive/v${version}.tar.gz"
+  url="$REPO/teleport-ent-${version}-bin.tar.gz"
 
   echo "* Downloading $TOOL_NAME release $version..."
   curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
